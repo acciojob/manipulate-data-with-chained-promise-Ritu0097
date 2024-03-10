32cd34949febd5c01af084877dfa236e3d1f96f0
Ritesh Kishor Chaudhari <100477678+Ritu0097@users.noreply.github.com>
2024-03-10 13:31:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,28 +5,22 @@ function processData(array) {
         }, 3000);
     });
 }
-function filterOddNumbers(array) {
+
+function filterEvenNumbers(array) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            const filteredArray = array.filter(num => num % 2 === 0);
-            resolve(filteredArray);
+            const evenNumbers = array.filter(num => num % 2 === 0);
+            resolve(evenNumbers);
         }, 1000);
     });
 }
-function multiplyEvenNumbers(array) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const multipliedArray = array.map(num => num % 2 === 0 ? num * 2 : num);
-            resolve(multipliedArray);
-        }, 2000);
-    });
-}
+
 const inputArray = [1, 2, 3, 4];
 
 processData(inputArray)
-    .then(filterOddNumbers)
-    .then(multiplyEvenNumbers)
-    .then(result => {
-        document.getElementById('output').textContent = result.join(', ');
+    .then(filterEvenNumbers)
+    .then(evenNumbers => {
+        const output = evenNumbers.join(', ');
+        document.getElementById('output').textContent = output;
     })
     .catch(error => console.error('An error occurred:', error));
